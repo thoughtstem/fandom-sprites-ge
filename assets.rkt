0efb67f25b4080c4b5a99a6c3a4dba62d2173c9b
sonnynajar <sonny.najar@gmail.com>
2020-01-15 08:38:20 -0800

    added pokemon-sprites.git as dep

--- assets.rkt
+++ assets.rkt
@@ -1,7 +1,8 @@
 #lang at-exp racket
 
 (require fandom-sheets
-         game-engine)
+         game-engine
+         pokemon-sprites)
 
 
 (provide ;Harry Potter
@@ -30,18 +31,7 @@
          diamond-sprite
          fireball-sheet
          ;Pokemon
-         pokeball2-sheet
-         dawnstone-sprite
-         duskstone-sprite
-         everstone-sprite
-         firestone-sprite
-         leafstone-sprite
-         moonstone-sprite
-         shinystone-sprite
-         sunstone-sprite
-         thunderstone-sprite
-         waterstone-sprite
-         dawnstone-sprite
+         (all-from-out pokemon-sprites)
          )
 
 (define (easy-sprite sheet r c n)
@@ -248,24 +238,7 @@
                                (physical-collider)))
 
 ;Pokemon
-(define-sprite armoredmewtwo-sheet 4 4 3 armoredmewtwo-sprite)
-(define-sprite pikachu-sheet 1 4 1 pikachu-sprite)
-(define-sprite pikachurun-sheet 1 4 1 pikachurun-sprite)
-(define-sprite jessie-sheet 4 4 3 jessie-sprite)
-(define-sprite james-sheet 4 4 3 james-sprite)
-(define-sprite redboy-sheet 4 4 3 redboy-sprite)
-(define-sprite redgirl-sheet 4 4 3 redgirl-sprite)
-(define-sprite greenboy-sheet 4 4 3 greenboy-sprite)
-(define-sprite greengirl-sheet 4 4 3 greengirl-sprite)
-(define-sprite bulbasaur-sheet 1 5 1 bulbasaur-sprite)
-(define-sprite ivysaur-sheet 1 6 1 ivysaur-sprite)
-(define-sprite venusaur-sheet 1 5 1 venasaur-sprite)
-(define-sprite squirtle-sheet 1 6 1 squirtle-sprite)
-(define-sprite wartortle-sheet 1 8 1 wartortle-sprite)
-(define-sprite blastoise-sheet 1 8 1 blastoise-sprite)
-(define-sprite charmander-sheet 1 5 1 charmander-sprite)
-(define-sprite charmeleon-sheet 1 8 1 charmelon-sprite)
-(define-sprite charizard-sheet 1 8 1 charizard-sprite)
+;pulled in from pokemon-sprites pkg
 
 ;Fortnite
 (define-sprite cecil-sheet 4 4 3 cecil-sprite)
--- info.rkt
+++ info.rkt
@@ -5,5 +5,6 @@
 
 (define deps '(
   "https://github.com/thoughtstem/fandom-sheets.git"
+  "https://github.com/thoughtstem/pokemon-sprites.git"
   ))
 
